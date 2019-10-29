d504660d78dc6c01083d0d06829e70f23ac6fec6
Jehad Kuhail <jehadkuhail1998@gmail.com>
2019-10-29 11:13:37 +0200

    ready

---
+++ app/src/main/java/com/jkuhail/android/volleyandglide/adapter/RvAdapter.java
@@ -0,0 +1,83 @@
+package com.jkuhail.android.volleyandglide.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.jkuhail.android.volleyandglide.R;
+import com.jkuhail.android.volleyandglide.model.Anime;
+
+import java.util.List;
+
+public class RvAdapter extends RecyclerView.Adapter<RvAdapter.MyViewHolder> {
+
+    RequestOptions options ;
+    private Context mContext ;
+    private List<Anime> mData ;
+
+
+    public RvAdapter(Context mContext, List lst) {
+
+
+        this.mContext = mContext;
+        this.mData = lst;
+        options = new RequestOptions()
+                .centerCrop()
+                .placeholder(R.drawable.ic_loading)
+                .error(R.drawable.ic_loading);
+
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view ;
+        LayoutInflater mInflater = LayoutInflater.from(mContext);
+        view = mInflater.inflate(R.layout.activity_main,parent,false);
+        // click listener here
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, final int position) {
+
+        holder.tvname.setText(mData.get(position).getName());
+        holder.tv_rate.setText(mData.get(position).getRating());
+        holder.tvstudio.setText(mData.get(position).getStudio());
+        holder.tvcat.setText(mData.get(position).getCategorie());
+
+        // load image from the internet using Glide
+        Glide.with(mContext).load(mData.get(position).getImage_url()).apply(options).into(holder.AnimeThumbnail);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvname,tv_rate,tvstudio,tvcat;
+        ImageView AnimeThumbnail;
+
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            tvname = itemView.findViewById(R.id.rowname);
+            tvstudio = itemView.findViewById(R.id.studio);
+            tv_rate = itemView.findViewById(R.id.rating);
+            tvcat = itemView.findViewById(R.id.categorie);
+            AnimeThumbnail = itemView.findViewById(R.id.thumbnail);
+        }
+    }
+
+
+}
